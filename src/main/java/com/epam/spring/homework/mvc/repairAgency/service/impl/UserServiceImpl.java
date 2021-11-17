97a2e47eb74127275697ce2063aecb9f1e905373
jnIOHwta <ntgraal@gmail.com>
2021-11-17 20:25:06 +0300

    Add mapping for User.

--- src/main/java/com/epam/spring/homework/mvc/repairAgency/service/impl/UserServiceImpl.java
+++ src/main/java/com/epam/spring/homework/mvc/repairAgency/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.epam.spring.homework.mvc.repairAgency.service.impl;
 
 import com.epam.spring.homework.mvc.repairAgency.controller.dto.UserDto;
 import com.epam.spring.homework.mvc.repairAgency.service.UserService;
+import com.epam.spring.homework.mvc.repairAgency.service.mapper.UserMapper;
 import com.epam.spring.homework.mvc.repairAgency.service.model.User;
 import com.epam.spring.homework.mvc.repairAgency.service.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +18,13 @@ import java.util.stream.Collectors;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     @Override
     public UserDto getUser(String login) {
         log.info("getUser by login {}", login);
         User user = userRepository.get(login);
-        return mapUserToUserDto(user);
+        return userMapper.mapUserDto(user);
     }
 
     @Override
@@ -30,24 +32,24 @@ public class UserServiceImpl implements UserService {
         log.info("get all users");
         return userRepository.getAll()
                 .stream()
-                .map(this::mapUserToUserDto)
+                .map(userMapper ::mapUserDto)
                 .collect(Collectors.toList());
     }
 
     @Override
     public UserDto createUser(final UserDto userDto) {
         log.info("createUser with email {}", userDto.getEmail());
-        User user = mapUserDtoToUser(userDto);
+        User user = userMapper.mapUser(userDto);
         user = userRepository.add(user);
-        return mapUserToUserDto(user);
+        return userMapper.mapUserDto(user);
     }
 
     @Override
     public UserDto updateUser(final String login, final UserDto userDto) {
         log.info("updateUser with login {}", login);
-        User user = mapUserDtoToUser(userDto);
+        User user = userMapper.mapUser(userDto);
         user = userRepository.update(login, user);
-        return mapUserToUserDto(user);
+        return userMapper.mapUserDto(user);
     }
 
     @Override
@@ -55,21 +57,4 @@ public class UserServiceImpl implements UserService {
         log.info("deleteUser by login {}", login);
         return userRepository.delete(login);
     }
-
-    private UserDto mapUserToUserDto(final User user) {
-        return UserDto.builder()
-                .name(user.getName())
-                .login(user.getLogin())
-                .email(user.getEmail())
-                .build();
-    }
-
-    private User mapUserDtoToUser(final UserDto userDto) {
-        return User.builder()
-                .name(userDto.getName())
-                .login(userDto.getLogin())
-                .email(userDto.getEmail())
-                .build();
-    }
-
 }
---
+++ src/main/java/com/epam/spring/homework/mvc/repairAgency/service/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.epam.spring.homework.mvc.repairAgency.service.mapper;
+
+import com.epam.spring.homework.mvc.repairAgency.controller.dto.UserDto;
+import com.epam.spring.homework.mvc.repairAgency.service.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.Set;
+
+@Mapper
+public interface UserMapper{
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+    Set<UserDto> mapUserDtos(Set<User> users);
+    UserDto mapUserDto(User users);
+    User mapUser(UserDto dto);
+}
