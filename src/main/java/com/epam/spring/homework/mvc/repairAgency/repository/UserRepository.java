519917fcb2b29a780fa284b7164c91e614efb163
jnIOHwta <ntgraal@gmail.com>
2021-11-21 22:00:07 +0300

    Delee redundant method.

--- src/main/java/com/epam/spring/homework/mvc/repairAgency/repository/UserRepository.java
+++ src/main/java/com/epam/spring/homework/mvc/repairAgency/repository/UserRepository.java
@@ -2,8 +2,6 @@ package com.epam.spring.homework.mvc.repairAgency.repository;
 
 import com.epam.spring.homework.mvc.repairAgency.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,8 +15,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findAll();
 
     User save(User user);
-
-    @Modifying
-    @Query("update User u set u.name = ?1 where u.id = ?2")
-    User updateName(String name, Long id);
 }
