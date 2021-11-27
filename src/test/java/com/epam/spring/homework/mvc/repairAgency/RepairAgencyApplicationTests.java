252076aa60d557d3015e0e14c59b39c13a8aded1
jnIOHwta <ntgraal@gmail.com>
2021-11-27 21:00:33 +0300

    Add upload the context test.

--- src/test/java/com/epam/spring/homework/mvc/repairAgency/RepairAgencyApplicationTests.java
+++ src/test/java/com/epam/spring/homework/mvc/repairAgency/RepairAgencyApplicationTests.java
@@ -1,13 +1,35 @@
 package com.epam.spring.homework.mvc.repairAgency;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 class RepairAgencyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @LocalServerPort
+    private int port;
+
+    @Value("${test.inject.property}")
+    private String password;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+
+    @Test
+    void contextLoads() {
+
+        HttpStatus statusCode = restTemplate.getForEntity("http://localhost:" + port + "/actuator/health", String.class).getStatusCode();
+        assertEquals(statusCode, HttpStatus.OK);
+    }
 
 }
