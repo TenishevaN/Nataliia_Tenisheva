405ef018f32e399b77aa673d075630cb1dda736b
jnIOHwta <ntgraal@gmail.com>
2021-11-29 01:54:21 +0300

    Add the tests for the methods of the InvoiceController.

--- src/test/java/com/epam/spring/homework/mvc/repairAgency/controller/InvoiceControllerTest.java
+++ src/test/java/com/epam/spring/homework/mvc/repairAgency/controller/InvoiceControllerTest.java
@@ -4,6 +4,7 @@ import com.epam.spring.homework.mvc.repairAgency.dto.InvoiceDto;
 import com.epam.spring.homework.mvc.repairAgency.service.InvoiceService;
 import com.epam.spring.homework.mvc.repairAgency.test.config.TestWebConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,12 +14,19 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(value = InvoiceController.class)
 @AutoConfigureMockMvc
@@ -32,6 +40,89 @@ public class InvoiceControllerTest {
     private InvoiceService invoiceService;
 
     private final ObjectMapper mapper = new ObjectMapper();
+
+    private static InvoiceDto invoiceDto;
+    private static InvoiceDto invoiceWithAmmountDto;
+
+    @BeforeAll
+    static void initializateValues() {
+
+        invoiceDto = InvoiceDto
+                .builder()
+                .build();
+
+        invoiceWithAmmountDto = InvoiceDto
+                .builder()
+                .ammount(BigDecimal.valueOf(200))
+                .build();
+    }
+
+    @Test
+    void addInvoiceTest() throws Exception {
+
+        given(invoiceService.add(any(InvoiceDto.class))).willReturn(invoiceWithAmmountDto);
+
+        mockMvc.perform(post("/invoice")
+                        .content(mapper.writeValueAsString(invoiceWithAmmountDto))
+                        .contentType(APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+    @Test
+    void updateInvoiceTest() throws Exception {
+
+        given(invoiceService.update(anyLong(), any(InvoiceDto.class))).willReturn(invoiceWithAmmountDto);
+
+        mockMvc.perform(put("/invoice/{id}", 1L)
+                        .content(mapper.writeValueAsString(invoiceWithAmmountDto))
+                        .contentType(APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
+
+
+    @Test
+    void getAllInvoicesTest() throws Exception {
+
+        when(invoiceService.getAll()).thenReturn(Arrays.asList(invoiceWithAmmountDto));
+
+        mockMvc.perform(get("/invoice"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$[0].ammount").value(invoiceWithAmmountDto.getAmmount()))
+                .andExpect(jsonPath("$").isArray());
+    }
+
+    @Test
+    void getInvoiceTest() throws Exception {
+
+        given(invoiceService.get(anyLong())).willReturn(invoiceWithAmmountDto);
+
+        mockMvc.perform(get("/invoice/{id}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(jsonPath("$.ammount").value(invoiceWithAmmountDto.getAmmount()));
+    }
+
+    @Test
+    void deleteInvoiceTest() throws Exception {
+
+
+        when(invoiceService.delete(anyLong())).thenReturn(true);
+
+        mockMvc.perform(delete("/invoice/{id}", 1L))
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
+    }
 
 
     @Test
@@ -56,10 +147,6 @@ public class InvoiceControllerTest {
 
     @Test
     void validateAmmountFieldNotNullTest() throws Exception {
-
-        InvoiceDto invoiceDto = InvoiceDto
-                .builder()
-                .build();
 
         given(invoiceService.add(any(InvoiceDto.class))).willReturn(invoiceDto);
 
