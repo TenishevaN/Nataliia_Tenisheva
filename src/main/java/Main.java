520d50192488c081a43cbc14fedaacb99269c6f6
jnIOHwta <ntgraal@gmail.com>
2021-12-01 15:58:42 +0300

    Add redundant lines to check a sonar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,15 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args){
+
+
+
+//        System.out.println("Hello");
+
+
+
+
+
         System.out.println("Hello");
 
     }
